fff091cc374e2ef9f5cd958634859c453ca3fdab
RobertoFORTs <bobfortesneto@gmail.com>
2023-08-16 08:25:49 -0400

    feat: skeleton of the project

---
+++ sorting.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sorting.h"
+
+//funções auxiliares
+
+//funções principais
+void selectionSort(){
+
+}
+
+void insertionSorte(){
+
+}
+
+void mergeSort(){
+
+}
+
+void heapSort(){
+
+}
+
+void quickSort(){
+
+}
+
+void countingSort(){
+    
+}
---
+++ sorting.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//funções auxiliares
+
+
+//funções principais
+void selectionSort();
+
+void insertionSort();
+
+void mergeSort();
+
+void heapSort();
+
+void quickSort();
+
+void countingSort();
