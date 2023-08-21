2855b51d695389ac70068637231a3b5860a48435
RobertoFORTs <bobfortesneto@gmail.com>
2023-08-21 11:18:35 -0400

    add insertion sort algorithm

--- sorting.c
+++ sorting.c
@@ -9,8 +9,17 @@ void selectionSort(){
 
 }
 
-void insertionSorte(){
+void insertionSort(int n, float *vetor){
+    int i, j;
+    float valor;
 
+    for (i = 1; i < n; i++){
+        valor = vetor[i];
+        for (j = i - 1; j >= 0 && vetor[j] > valor; j--){
+            vetor[j+1] = vetor[j]; 
+        }
+        vetor[j+1] = valor;
+    }
 }
 
 void mergeSort(){
