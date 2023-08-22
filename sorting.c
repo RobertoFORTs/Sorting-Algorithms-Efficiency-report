ce9d8c83b30174430ac8bc7c8ea2a962cf46dc0a
RobertoFORTs <bobfortesneto@gmail.com>
2023-08-22 16:30:01 -0400

    feat: add intercala for implementing mergesort

--- sorting.c
+++ sorting.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sorting.h"
+#define MAX 20000
 
 //funções auxiliares
+void intercala(int p, int q, int r, int v[MAX]){
+  int i, j, k, w[MAX];
+
+  i = p; j = q; k = 0; 
+  while(i < q && j < r) {
+    if(v[i] < v[j]){
+      w[k] = v[i]; i++;
+    }
+    else{
+      w[k] = v[j]; j++;
+    }
+    k++;
+  }
+  while(i < q){
+    w[k] = v[i]; i++; k++;
+    }
+  while(j < r){
+    w[k] = v[j]; j++; k++;  
+  }
+  for(i = p; i < r; i++) {
+    v[i] = w[i - p];
+  }
+}
 
 //funções principais
 void selectionSort(){
