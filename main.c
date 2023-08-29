1f48226951d9dda9bd690135ffd2c0fc21118954
RobertoFORTs <bobfortesneto@gmail.com>
2023-08-29 16:35:47 -0400

    add more structure to analysing algorithm efficiency

--- main.c
+++ main.c
@@ -17,12 +17,12 @@ int main(){
     int quantidadeDeN = (fim - inc)/stp;
     int n = inc;
     srand(time(NULL));
-    float somaMerge;
+    float somaInsertion, somaSelection, somaMerge, somaQuick, somaHeap, somaCount;
 
     //---------------------------------------------------------
     //Primeiro Caso (execução dos processos repetidas vezes)
     printf("Primeiro caso, Vetor preenchido aleatóriamente:\n");
-    printf("            Mergesort");
+    printf("            InsertionSort     SelectionSort     Mergesort     Quicksort     HeapSort     CountingSort     ");
     while (i <= quantidadeDeN && n <= fim){
         
         j = 0; //controla a quantidade de repetições para cada caso de teste
@@ -33,24 +33,55 @@ int main(){
             int x = rand();
             if (x >= 0)
                 v[aux] = x;
-            
         }
-
+        
         while (j < rpt){ //realizar operações com vetor atual
             
-            clock_t mergeBegin = clock();
+            clock_t insertionBegin = clock();
             insertionSort(n, v);
-            clock_t mergeEnd = clock();
+            clock_t insertionEnd = clock();
             
+            clock_t selectionBegin = clock();
+            selectionSort(v, n);
+            clock_t selectionEnd = clock();
+            
+            // clock_t mergeBegin = clock();
+            // mergeSort(0, n, v);
+            // clock_t mergeEnd = clock();
+
+            clock_t quickBegin = clock();
+            quickSort(v, 0, n);
+            clock_t quickEnd = clock();
+
+            // clock_t heapBegin = clock();
+            // heapSort();
+            // clock_t heapEnd = clock();
+            
+            // clock_t countBegin = clock();
+            // countingSort();
+            // clock_t countEnd = clock();
+
             //obter soma dos tempos de execução para cada algorítimo
-            somaMerge = somaMerge + ((float)mergeEnd - (float)mergeBegin) / 1000000.0F;
+            somaInsertion = somaInsertion + ((float)insertionEnd - (float)insertionBegin) / 1000000.0F;
+            somaSelection = somaSelection + ((float)selectionEnd - (float)selectionBegin) / 1000000.0F;
+            //somaMerge = somaMerge + ((float)mergeEnd - (float)mergeBegin) / 1000000.0F;
+            somaQuick = somaQuick + ((float)quickEnd - (float)quickBegin) / 1000000.0F;
+            //somaHeap = somaHeap + ((float)heapEnd - (float)heapBegin) / 1000000.0F;
+            //somaCount = somaCount + ((float)countEnd - (float)countBegin) / 1000000.0F;
+
+            
 
             j++;
         }
         //calcular media para cada n e exibir
-        float mediaMerge = somaMerge/rpt;
+        float mediaInsertion = somaInsertion/rpt;
+        float mediaSelection = somaSelection/rpt;
+        // float mediaMerge = somaMerge/rpt;
+        float mediaQuick = somaQuick/rpt;
+        // float mediaHeap = somaHeap/rpt;
+        // float mediaCount = somaCount/rpt;
         
-        printf("\nn:%d        %f\n", n, mediaMerge);
+        printf("\nn:%d        %f          %f         merge         %f\n", n, mediaInsertion, mediaSelection, mediaQuick);
         free(v); //liberando vetor
         n = n + stp;
         i++;
