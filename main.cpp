3a396eaadb85a076e0f580ec9b86f3ed9f5a5b0e
RobertoFORTs <bobfortesneto@gmail.com>
2023-08-30 22:52:25 -0400

    feat: add new tests

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(){
     //---------------------------------------------------------
     //Primeiro Caso (execução dos processos repetidas vezes)
     printf("\n");
-    printf("PRIMEIRO CASO: Vetor preenchido aleatóriamente:\n\n");
+    printf("[ [RANDOM] ]\n");
     printf("               InsertionSort     SelectionSort    Mergesort        Quicksort        HeapSort         CountingSort\n");
     while (i <= quantidadeDeN && n <= fim){
         
@@ -36,7 +36,7 @@ int main(){
         int *w = (int*)malloc((n+1)* sizeof(int)); // vetor controle (copia)
         while (j < rpt){ //realizar operações com vetor atual
 
-            while (aux < n){ //preencher vetor
+            while (aux < n){ //preencher vetor e copiar
                 int x = rand();
                 if (x >= 0){
                     v[aux] = x;
@@ -123,16 +123,19 @@ int main(){
     // Para os restante dos casos, utilizar os mesmos valores de entrada, porém com apenas uma execução para cada vetor de tamanho n
     
     i = 0;
-    printf("\nSEGUNDO CASO: Vetor Ordenado Decrescentemente\n\n");
+    printf("\n\n[ [REVERSE] ]\n");
     printf("               InsertionSort     SelectionSort    Mergesort        Quicksort        HeapSort         CountingSort\n");
     n = inc;
     while (i <= quantidadeDeN){
         int *v = (int *)malloc((n+1)*sizeof(int));
         int *w = (int *)malloc((n+1)*sizeof(int));
-        for (aux = 0; aux < n; aux++){ //preencher vetor
+        aux = 0;
+        while (aux < n){ //preencher vetor
             int x = rand();
-            if (x >= 0)
+            if (x >= 0){
                 v[aux] = x;
+                aux++;
+            }
         }
         
         mergeSort(0, n, v); //Primeiramente, ordena
@@ -205,17 +208,150 @@ int main(){
 
     //---------------------------------------
     i = 0;
-    printf("terceiro caso\n");
+    printf("\n\n[ [SORTED] ]\n");
+    printf("               InsertionSort     SelectionSort    Mergesort        Quicksort        HeapSort         CountingSort\n");
+    n = inc;
     while (i <= quantidadeDeN){
+        int *v = (int *)malloc((n+1)*sizeof(int));
+        int *w = (int *)malloc((n+1)*sizeof(int));
+        aux = 0;
+        while (aux < n){ //preencher vetor
+            int x = rand();
+            if (x >= 0){
+                v[aux] = x;
+                aux++;
+            }
+        }
+        
+        mergeSort(0, n, v); //Primeiramente, ordena
+        //não precisa de cópia pois basta um vetor ordenado
 
+        clock_t insertionBegin = clock();
+        insertionSort(n, v);
+        clock_t insertionEnd = clock();
+        timeInsertion = ((float)insertionEnd - (float)insertionBegin)/CLOCKS_PER_SEC;
+
+        clock_t selectionBegin = clock();
+        selectionSort(v, n);
+        clock_t selectionEnd = clock();
+        timeSelection = ((float)selectionEnd - (float)selectionBegin)/CLOCKS_PER_SEC;
+    
+
+        clock_t mergeBegin = clock();
+        mergeSort(0, n, v);
+        clock_t mergeEnd = clock();
+        timeMerge = ((float)mergeEnd - (float)mergeBegin)/CLOCKS_PER_SEC;
+
+        clock_t quickBegin = clock();
+        quickSort(v, 0, n);
+        clock_t quickEnd = clock();
+        timeQuick = ((float)quickEnd - (float)quickBegin)/CLOCKS_PER_SEC;
+
+        clock_t heapBegin = clock();
+        heapSort(n, v);
+        clock_t heapEnd = clock();
+        timeHeap = ((float)heapEnd - (float)heapBegin)/CLOCKS_PER_SEC;
+    
+
+        // clock_t countBegin = clock();
+        // countingSort();
+        // clock_t countEnd = clock();
+
+        printf("\nn:%d        %f          %f         %f         %f         %f         count\n", n, timeInsertion, timeSelection, timeMerge, timeQuick, timeHeap);
+
+        free(v);
+        free(w);
+        n = n + stp;
         i++;
     }
 
     //----------------------------------------
     i = 0;
-    printf("Quarto caso \n");
+    printf("\n\n[ [NEARLY SORTED] ]\n");
+    printf("               InsertionSort     SelectionSort    Mergesort        Quicksort        HeapSort         CountingSort\n");
+    n = inc;
     while (i <= quantidadeDeN){
+        int *v = (int *)calloc((n+1), sizeof(int));
+        int *w = (int *)calloc((n+1), sizeof(int));
+        aux = 0;
+        while (aux < (90*n)/100){ //preencher 90% do vetor
+            int x = rand();
+            if (x >= 0){
+                v[aux] = x;
+                aux++; 
+            }
 
+        }
+        
+        mergeSort(0, n, v); //Primeiramente, ordena os 90%
+        
+        while (aux < n){
+            int x = rand();
+            if (x >= 0){
+                v[aux] = x;
+                aux++; 
+            }
+        } // vetor parcialmente ordenado pronto
+
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+        
+
+        clock_t insertionBegin = clock();
+        insertionSort(n, w);
+        clock_t insertionEnd = clock();
+        timeInsertion = ((float)insertionEnd - (float)insertionBegin)/CLOCKS_PER_SEC;
+
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+
+        clock_t selectionBegin = clock();
+        selectionSort(w, n);
+        clock_t selectionEnd = clock();
+        timeSelection = ((float)selectionEnd - (float)selectionBegin)/CLOCKS_PER_SEC;
+        
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+
+        clock_t mergeBegin = clock();
+        mergeSort(0, n, w);
+        clock_t mergeEnd = clock();
+        timeMerge = ((float)mergeEnd - (float)mergeBegin)/CLOCKS_PER_SEC;
+
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+
+        clock_t quickBegin = clock();
+        quickSort(w, 0, n);
+        clock_t quickEnd = clock();
+        timeQuick = ((float)quickEnd - (float)quickBegin)/CLOCKS_PER_SEC;
+
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+
+        clock_t heapBegin = clock();
+        heapSort(n, w);
+        clock_t heapEnd = clock();
+        timeHeap = ((float)heapEnd - (float)heapBegin)/CLOCKS_PER_SEC;
+        
+        for (aux = 0; aux < n; aux++){
+            w[aux] = v[aux]; //cópia controle
+        }
+
+        // clock_t countBegin = clock();
+        // countingSort();
+        // clock_t countEnd = clock();
+
+        printf("\nn:%d        %f          %f         %f         %f         %f         count\n", n, timeInsertion, timeSelection, timeMerge, timeQuick, timeHeap);
+
+        free(v);
+        free(w);
+        n = n + stp;
         i++;
     }
 }
